d8ebb73618903932aade2924c65f2f94366e4587
Nicholas Whittier <imperialwicket@gmail.com>
2012-06-26 15:47:46 -0600

    Removes the manual binary to list conversion of password hash, and casts the value to a string in the module declaration.

--- src/model/colosimo_user.erl
+++ src/model/colosimo_user.erl
@@ -1,4 +1,4 @@
--module(colosimo_user, [Id, Email, Username, Password]).
+-module(colosimo_user, [Id, Email, Username, Password::string()]).
 -compile(export_all).
 
 -define(SETEC_ASTRONOMY, "Too many secrets").
@@ -7,8 +7,7 @@ session_identifier() ->
     mochihex:to_hex(erlang:md5(?SETEC_ASTRONOMY ++ Id)).
 
 check_password(PasswordAttempt) ->
-    StoredPassword = erlang:binary_to_list(Password),
-    StoredPassword =:= bcrypt:hashpw(PasswordAttempt, StoredPassword).
+    Password =:= bcrypt:hashpw(PasswordAttempt, Password).
 
 login_cookies() ->
     [ mochiweb_cookies:cookie("colosimo_user_id", Id, [{path, "/"}]),
